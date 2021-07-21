0b94e4d093a36aa30efdb20b294bebe08d25cbf6
Malo Kerebel <malo.kerebel@laposte.net>
2021-07-21 17:32:51 +0200

    The bot will now check if the tweet text contains what is searched

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
-import json
 import tweepy
 import time
 import os
 import csv
 import configparser
-import urllib.parse
 import sys
 from datetime import datetime
+
 
 def duplicate_check(id):
     value = False
@@ -18,6 +17,7 @@ def duplicate_check(id):
     f.close()
     return value
 
+
 def log_post(id, post_url):
     with open(CACHE_CSV, 'a', newline='') as cache:
         date = time.strftime("%Y/%m/%d") + ' ' + time.strftime("%H:%M:%S")
@@ -25,39 +25,69 @@ def log_post(id, post_url):
         wr.writerow([id, date, post_url])
     cache.close()
 
+
 def hour():
 
-	now = datetime.now()
-	hour = str(now.hour) + ':' + str(now.minute)
+    now = datetime.now()
+    hour = str(now.hour) + ':' + str(now.minute)
 
-	return hour
+    return hour
+
 
 def retweet(Search, Message):
 
-	tweet_list = []
-	#The 'recent' can be change to 'mixed' or to 'popular' to get different result
-	tweet_list = twitter.search(Search, result_type = 'recent', count = 1)
-	try:
-		tweet = tweet_list[0]
-		#Given the amount of tweets the duplicate check shouldn't be necessary but if you search for a really precise term it might be usefull
-		if (duplicate_check(tweet.id) == False):
-			try:
-				#twitter.retweet(tweet.id) could also works, the code below quote the tweets instead
-				retweet = twitter.update_status(Message + "\n\n" + "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str)
-				log_post(tweet.id, 'https://twitter.com/' + twitter.me().screen_name + '/status/' + retweet.id_str + '/')
-				print("[ OK ] Succesfully retweeted")			
-			except:
+    tweet_list = []
+    # The 'recent' can be change to 'mixed' or to 'popular'
+    # to get different result
+    tweet_list = twitter.search(Search, result_type='recent', count=1)
+    Search_split = Search.split()
+    Search_text = []
+    print(Search_split)
+    for i in Search_split:
+        print(i)
+        if not "filter" in i:
+            Search_text.append(i)
+    
+    try:
+        i = 0
+        found = False
+        while not found and i < len(tweet_list):
+            tweet = tweet_list[i]
+            found = True
+            for word in Search_text:
+                if word not in tweet.text:
+                    found = False
+            i += 1
+            
+        # Given the amount of tweets the duplicate check shouldn't be necessary
+        # but if you search for a really precise term it might be usefull
+        if (duplicate_check(tweet.id) is False):
+            try:
+                # twitter.retweet(tweet.id) could also works,
+                # the code below quotes the tweets instead
+                # It quotes the tweet by recreating the tweet URL
+                retweet = twitter.update_status(Message +
+                                                "\n\n" + "https://twitter.com/"
+                                                + tweet.user.screen_name
+                                                + "/status/" + tweet.id_str)
+                # log_post(tweet.id, 'https://twitter.com/'
+                #                  + twitter.me().screen_name
+                #                  + '/status/'
+                #                  + retweet.id_str + '/')
+                print("[ OK ] Succesfully retweeted")
+            except BaseException:
+                print(" [ ERROR ] Error while retweeting")
+                log_post(tweet.id, "Error while retweeting")
+            else:
+                print("[OK] this tweet has already been retweeted")
+    except BaseException:
+        # For some reason sometimes the 0th element of tweet_list
+        # is out of bounds and throw this error,
+        # I don't know how to fix this
+        print("[ ERROR ] An unknow error has occur")
 
-				print(" [ ERROR ] Error while retweeting")
-				log_post(tweet.id, "Error while retweeting")
-				
-		else:			
-			print("[OK] this tweet has already been retweeted")
-	except:
-		#For some reason sometimes the 0th element of tweet_list is out of bounds and throw this error, don't know how to fix this
-		print("[ ERROR ] An unknow error has occur")
 
-#The part below was made by corbindavenport
+# The part below was made by corbindavenport
 # Make sure config file exists
 try:
     config = configparser.ConfigParser()
@@ -74,73 +104,72 @@ MESSAGE = config['BotSettings']['Message']
 # Log into Twitter if enabled in settings
 
 if os.path.exists('twitter.secret'):
-# Read API keys from secret file
-	twitter_config = configparser.ConfigParser()
-	twitter_config.read('twitter.secret')
-	ACCESS_TOKEN = twitter_config['Twitter']['AccessToken']
-	ACCESS_TOKEN_SECRET = twitter_config['Twitter']['AccessTokenSecret']
-	CONSUMER_KEY = twitter_config['Twitter']['ConsumerKey']
-	CONSUMER_SECRET = twitter_config['Twitter']['ConsumerSecret']
-	try:
-	    # Make sure authentication is working
-	    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-	    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-	    twitter = tweepy.API(auth)
-	    twitter_username = twitter.me().screen_name
-	    print('[ OK ] Sucessfully authenticated on Twitter as @' + twitter_username)
-	except BaseException as e:
-	    print('[EROR] Error while logging into Twitter:', str(e))
-	    print('[EROR] Tootbot cannot continue, now shutting down')
-	    exit()
+    # Read API keys from secret file
+    twitter_config = configparser.ConfigParser()
+    twitter_config.read('twitter.secret')
+    ACCESS_TOKEN = twitter_config['Twitter']['AccessToken']
+    ACCESS_TOKEN_SECRET = twitter_config['Twitter']['AccessTokenSecret']
+    CONSUMER_KEY = twitter_config['Twitter']['ConsumerKey']
+    CONSUMER_SECRET = twitter_config['Twitter']['ConsumerSecret']
+    try:
+        # Make sure authentication is working
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        twitter = tweepy.API(auth)
+        twitter_username = twitter.me().screen_name
+        print('[ OK ] Sucessfully authenticated on Twitter as @' + twitter_username)
+    except BaseException as e:
+        print('[EROR] Error while logging into Twitter:', str(e))
+        print('[EROR] Tootbot cannot continue, now shutting down')
+        exit()
 else:
-# If the secret file doesn't exist, it means the setup process hasn't happened yet
-	print('[WARN] API keys for Twitter not found. Please enter them below (see wiki if you need help).')
-	ACCESS_TOKEN = ''.join(
-	    input('[ .. ] Enter access token for Twitter account: ').split())
-	ACCESS_TOKEN_SECRET = ''.join(
-	    input('[ .. ] Enter access token secret for Twitter account: ').split())
-	CONSUMER_KEY = ''.join(
-	    input('[ .. ] Enter consumer key for Twitter account: ').split())
-	CONSUMER_SECRET = ''.join(
-	    input('[ .. ] Enter consumer secret for Twitter account: ').split())
-	print('[ OK ] Attempting to log in to Twitter...')
-	try:
-		# Make sure authentication is working
-		auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-		auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-		twitter = tweepy.API(auth)
-		twitter_username = twitter.me().screen_name
-		print('[ OK ] Sucessfully authenticated on Twitter as @' + twitter_username)
-		# It worked, so save the keys to a file
-		twitter_config = configparser.ConfigParser()
-		twitter_config['Twitter'] = {
-			'AccessToken': ACCESS_TOKEN,
-			'AccessTokenSecret': ACCESS_TOKEN_SECRET,
-			'ConsumerKey': CONSUMER_KEY,
-			'ConsumerSecret': CONSUMER_SECRET
-		}
-		with open('twitter.secret', 'w') as f:
-			twitter_config.write(f)
-		f.close()
-	except BaseException as e:
-		print('[EROR] Error while logging into Twitter:', str(e))
-		print('[EROR] Tootbot cannot continue, now shutting down')
-		exit()
-
+    # If the secret file doesn't exist, it means the setup process hasn't happened yet
+    print('[WARN] API keys for Twitter not found. Please enter them below (see wiki if you need help).')
+    ACCESS_TOKEN = ''.join(
+        input('[ .. ] Enter access token for Twitter account: ').split())
+    ACCESS_TOKEN_SECRET = ''.join(
+        input('[ .. ] Enter access token secret for Twitter account: ').split())
+    CONSUMER_KEY = ''.join(
+        input('[ .. ] Enter consumer key for Twitter account: ').split())
+    CONSUMER_SECRET = ''.join(
+        input('[ .. ] Enter consumer secret for Twitter account: ').split())
+    print('[ OK ] Attempting to log in to Twitter...')
+    try:
+        # Make sure authentication is working
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        twitter = tweepy.API(auth)
+        twitter_username = twitter.me().screen_name
+        print('[ OK ] Sucessfully authenticated on Twitter as @' + twitter_username)
+        # It worked, so save the keys to a file
+        twitter_config = configparser.ConfigParser()
+        twitter_config['Twitter'] = {
+            'AccessToken': ACCESS_TOKEN,
+            'AccessTokenSecret': ACCESS_TOKEN_SECRET,
+            'ConsumerKey': CONSUMER_KEY,
+            'ConsumerSecret': CONSUMER_SECRET
+        }
+        with open('twitter.secret', 'w') as f:
+            twitter_config.write(f)
+            f.close()
+    except BaseException as e:
+        print('[EROR] Error while logging into Twitter:', str(e))
+        print('[EROR] Tootbot cannot continue, now shutting down')
+        exit()
 
 # Run the main script
 while True:
-	#Make a cache file so to store
-	if not os.path.exists(CACHE_CSV):
-		with open(CACHE_CSV, 'w', newline='') as cache:
-			default = ['Status ID', 'Date and time', 'Status link']
-			wr = csv.writer(cache)
-			wr.writerow(default)
-			print('[ OK ] ' + CACHE_CSV + ' file not found, created a new one')
-			cache.close()
+    # Make a cache file so to store
+    if not os.path.exists(CACHE_CSV):
+        with open(CACHE_CSV, 'w', newline='') as cache:
+            default = ['Status ID', 'Date and time', 'Status link']
+            wr = csv.writer(cache)
+            wr.writerow(default)
+            print('[ OK ] ' + CACHE_CSV + ' file not found, created a new one')
+            cache.close()
 
-	#Main process
-	retweet(SEARCH, MESSAGE)
-	print(hour() + ' [ OK ] Sleeping for', DELAY_BETWEEN_TWEETS, 'seconds')
-	time.sleep(DELAY_BETWEEN_TWEETS)
-	print('[ OK ] Restarting main process...')
+    # Main process
+    retweet(SEARCH, MESSAGE)
+    print(hour() + ' [ OK ] Sleeping for', DELAY_BETWEEN_TWEETS, 'seconds')
+    time.sleep(DELAY_BETWEEN_TWEETS)
+    print('[ OK ] Restarting main process...')
